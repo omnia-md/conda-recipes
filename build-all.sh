53516dbfdea64419610814d1acc0088ef5f0c394
Robert McGibbon <rmcgibbo@gmail.com>
2014-03-17 16:53:03 -0700

    add to build-all script

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,22 @@
 echo 'Building python2.7 and python3.3 conda packages'
 echo 'This will take a while... (15 minutes or more)'
 
+for CONDA_PY in 27 33; do
+    export CONDA_PY=$CONDA_PY
+    for CONDA_NPY in 17 18; do
+        export CONDA_NPY=$CONDA_NPY
+        conda build fastcluster
+        conda build mtraj
+    done
+done
+
+for CONDA_NPY in 17 18; do
+  export CONDA_PY=27
+  export CONDA_NPY=$CONDA_NPY
+  conda build msmbuilder
+done
+
+
 conda build fftw3f
 for CONDA_PY in 27 33; do
     export CONDA_PY=$CONDA_PY
