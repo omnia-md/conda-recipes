75edea7ddee94644eae50d004648fbda12f9ad5d
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-04-14 20:55:41 -0400

    Builds, but tests do not pass.

--- ambermini/build.sh
+++ ambermini/build.sh
@@ -4,7 +4,11 @@ export CFLAGS="-I$PREFIX/include $CFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
 # Configure build
+chmod u+x configure
 ./configure --prefix=$PREFIX
 
 # Build and install.
+make
+chmod -R u+x bin
 make install
+
