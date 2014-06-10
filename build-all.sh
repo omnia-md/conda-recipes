57816daa3de077d574aea19e7f0ed406eb30661d
vagrant <vagrant@lucid64.hsd1.ca.comcast.net>
2014-06-10 22:44:07 +0200

    improve build script not to rebuild things that arent needed

--- build-all.sh
+++ build-all.sh
@@ -1,34 +1,39 @@
 echo 'Building python2.7 and python3.3 conda packages'
 echo 'This will take a while... (15 minutes or more)'
 
+function build {
+    output=`conda build --output $1`
+    if [[ ! -f $output ]]; then
+	conda build $1
+    fi
+}
+
 for CONDA_PY in 27 33; do
     export CONDA_PY=$CONDA_PY
     for CONDA_NPY in 17 18; do
         export CONDA_NPY=$CONDA_NPY
-        conda build fastcluster
-        conda build mdtraj
+        build fastcluster
+        build mdtraj
     done
 done
-
-exit
 
 for CONDA_NPY in 17 18; do
   export CONDA_PY=27
   export CONDA_NPY=$CONDA_NPY
-  conda build msmbuilder
+  build msmbuilder
 done
 
 
 conda build fftw3f
 for CONDA_PY in 27 33; do
     export CONDA_PY=$CONDA_PY
-    conda build scripttest
-    conda build openmm
-    conda build mdtraj
+    build scripttest
+    build openmm
+    build mdtraj
 done
 
-conda build ambermini
-conda build pdbfixer
+build ambermini
+build pdbfixer
 
 
 
