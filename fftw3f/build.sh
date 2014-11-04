f7e8b42fa8a8224d55a423fddeba444bf9a89654
Robert McGibbon <rmcgibbo@gmail.com>
2014-11-03 20:42:12 -0800

    fix?

--- fftw3f/build.sh
+++ fftw3f/build.sh
@@ -1,10 +1,6 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # without manually specifying arch i386 and x86_64, we don't get a 
-    # universal binary on OSX
-    ./configure --prefix=$PREFIX --enable-shared --disable-fortran \
-	--enable-threads --enable-sse2 --enable-single \
-	CC="gcc -arch i386 -arch x86_64" CXX="g++ -arch i386 -arch x86_64" \
-	CPP="gcc -E" CXXCPP="g++ -E"
+    ./configure --prefix=$PREFIX --enable-shared --disable-fortran --disable-static \
+	--enable-threads --enable-sse2 --enable-single
 else
     ./configure --prefix=$PREFIX --enable-shared --disable-fortran \
 	--enable-threads --enable-sse2 --enable-single
