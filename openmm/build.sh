f1e7cf2d2775cf9d565f4b0a6e18f1d985254272
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-11-03 20:44:34 -0500

    Updated OpenMM recipe.

--- openmm/build.sh
+++ openmm/build.sh
@@ -6,19 +6,17 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # setting the rpath so that libOpenMMPME.so finds the right libfftw3
     CMAKE_FLAGS+=" -DCMAKE_INSTALL_RPATH=.."
     CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
-    CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=/usr/local/cuda-6.5/include"
-    CMAKE_FLAGS+=" -DOPENCL_LIBRARY=/usr/local/cuda-6.5/lib64/libOpenCL.so"
-    #CMAKE_FLAGS+=" -DOPENCL_LIBRARY=/opt/AMDAPP/lib/x86_64/libOpenCL.so"
-
+    CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=/usr/local/cuda-6.5/include" # CUDA 6.5
+    CMAKE_FLAGS+=" -DOPENCL_LIBRARY=/usr/local/cuda-6.5/lib64/libOpenCL.so" # CUDA 6.5
+    CMAKE_FLAGS+=" -DCUDA_CUDART_LIBRARY=/usr/local/cuda-6.5/lib64/libcudart.so"
+    CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=/usr/local/cuda-6.5/bin/nvcc"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_INCLUDE=/usr/local/cuda-6.5/include"
+    CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-6.5"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export MACOSX_DEPLOYMENT_TARGET="10.9"
-    #CMAKE_FLAGS+=" -DCMAKE_VERBOSE_MAKEFILE=TRUE" # for debugging
-    CMAKE_FLAGS+=" -DOSX_DEPLOYMENT_TARGET=10.9" # builds are on OS X 10.9
-    CMAKE_FLAGS+=" -DOSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" # builds are on OS X 10.9
-    CMAKE_FLAGS+=" -DCMAKE_OSX_ARCHITECTURES=x86_64" # CUDA 6.5 no longer supports 32-bit
-    CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++" # use clang compilers
-    CMAKE_FLAGS+=" -DOPENMM_BUILD_DRUDE_OPENCL_LIB=OFF" # OpenCL is only partially disabled on OS X, so turn this off manually
-    CMAKE_FLAGS+=" -DOPENMM_BUILD_RPMD_OPENCL_LIB=OFF" # OpenCL is only partially disabled on OS X, so turn this off manually
+    CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+    CMAKE_FLAGS+=" -DOPENMM_BUILD_OPENCL_LIB=OFF -DOPENMM_BUILD_DRUDE_OPENCL_LIB=OFF -DOPENMM_BUILD_RPMD_OPENCL_LIB=OFF -DOPENMM_BUILD_OPENCL_TESTS=FALSE -DOPENMM_BUILD_OPENCL_DOUBLE_PRECISION_TESTS=FALSE" # Don't build OpenCL on OS X
 fi
 
 # Set location for FFTW3 on both linux and mac
@@ -31,16 +29,31 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     CMAKE_FLAGS+=" -DFFTW_THREADS_LIBRARY=$PREFIX/lib/libfftw3f_threads.dylib"
 fi
 
+# Copy source to current directory.
+cp -r $RECIPE_DIR/../.. .
+
+# Build in subdirectory.
 mkdir build
 cd build
-cmake .. ${CMAKE_FLAGS}
-make -j8
+cmake .. $CMAKE_FLAGS
+make -j16 all DoxygenApiDocs sphinxpdf # build docs as well
 make install
 
-export LD_LIBRARY_PATH="/opt/gnu/gcc/4.8.1/lib64:/opt/gnu/gcc/4.8.1/lib:/opt/gnu/gmp/lib:/opt/gnu/mpc/lib:/opt/gnu/mpfr/lib"
+# Run C tests.
+# Exclude OpenCL tests because @peastman suspects mesa on travis implementation is broken.
+# @jchodera and @pgrinaway suspect travis is working, but AMD OpenCL tests are actually failing due to a bug.
+#ctest -j2 -V -E "[A-Za-z]+OpenCL[A-Za-z]+"
 
+# Install Python wrappers.
 export OPENMM_INCLUDE_PATH=$PREFIX/include
 export OPENMM_LIB_PATH=$PREFIX/lib
 cd python
-#$PYTHON setup.py config --compiler=clang build --compiler=clang install
 $PYTHON setup.py install
+cd ..
+
+# Remove one random file
+#rm $PREFIX/bin/TestReferenceHarmonicBondForce
+
+# Copy all tests to bin directory so they will be distributed with install package.
+#cp `find . -name "Test*" -type f -maxdepth 1` $PREFIX/bin
+
