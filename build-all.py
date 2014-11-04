af4260a8e626a33363d356619988adbf73fd6b94
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-11-03 22:31:55 -0500

    Updated build script to provide more debug output.
    Former-commit-id: c5ab0e91b50c5d459942eb44f13c8241327daa40

--- build-all.py
+++ build-all.py
@@ -7,7 +7,9 @@ print('Building python2.7 and python3.3 conda packages')
 print('This will take a while... (15 minutes or more)')
 
 def build(name):
-    fn = shell('conda build --output %s' % name)
+    command = 'conda build --output %s' % name
+    print(command)
+    fn = shell(command)
     if not exists(fn):
         shell('conda build %s' % name)
 
