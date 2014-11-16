991048c709c27f39d00502f56dc5c12e4ac16447
Martin K. Scherer <m.scherer@fu-berlin.de>
2014-11-16 18:53:24 +0100

    enable workaround on mavericks or higher

--- jpype1/build.sh
+++ jpype1/build.sh
@@ -3,17 +3,16 @@
 s="import platform; import sys
 from distutils.version import StrictVersion
 curr = StrictVersion(platform.mac_ver()[0])
-yosemite = StrictVersion('10.10')
-print curr, yosemite
-if curr >= yosemite:
+mavericks = StrictVersion('10.9')
+if curr >= mavericks:
    sys.exit(0)
 else:
    sys.exit(1)
 "
 
-# if on osx yosemite or higher link against new libstdc++
+# if on osx mavericks or higher link against new libstdc++
 if `$PYTHON -c "$s"`; then
- echo "on yosemite"
+ echo "on mavericks or higher"
  export CFLAGS="-stdlib=libstdc++"
 fi
 
