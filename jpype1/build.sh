a56c56027466a698164e25de0bdd0beee718b918
Martin K. Scherer <m.scherer@fu-berlin.de>
2014-11-16 18:08:10 +0100

    [jpype1] check for presence of yosemite and link against new c++ lib

--- jpype1/build.sh
+++ jpype1/build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+
+s="import platform; import sys
+from distutils.version import StrictVersion
+curr = StrictVersion(platform.mac_ver()[0])
+yosemite = StrictVersion('10.10')
+if curr >= yosemite:
+   sys.exit(0)
+else:
+   sys.exit(1)
+"
+
+# if on osx yosemite or higher link against new libstdc++
+if `$PYTHON -c "$s"`; then
+ echo "on yosemite";
+ CFLAGS="-stdlib=libstdc++"
+fi
+
 $PYTHON setup.py install
