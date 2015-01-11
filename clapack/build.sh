454020ecb2052fdafdc4e9909aac6ea28dc8c380
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-11 00:29:34 -0800

    Simplify
    Former-commit-id: ac783a38f5a7049c90046736a58d04ec7c2be469

--- clapack/build.sh
+++ clapack/build.sh
@@ -1,11 +1,7 @@
 mkdir build
 cd build
-CMAKE_FLAGS=" -DCMAKE_INSTALL_PREFIX=$PREFIX"
-CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
-CMAKE_FLAGS+=" -Wno-dev"
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "hello";
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -Wno-dev"
+if [[ "$OSTYPE" == "darwin"* ]]; then
     CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=''"
 fi
 
