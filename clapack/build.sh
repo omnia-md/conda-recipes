3074e148aeaa3aedf83aef0c638d9bb04637d83c
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-11 00:26:17 -0800

    Add osx

--- clapack/build.sh
+++ clapack/build.sh
@@ -1,1 +1,13 @@
-exit 1
+mkdir build
+cd build
+CMAKE_FLAGS=" -DCMAKE_INSTALL_PREFIX=$PREFIX"
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
+CMAKE_FLAGS+=" -Wno-dev"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "hello";
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=''"
+fi
+
+cmake $CMAKE_FLAGS ..
+make -j4 install
