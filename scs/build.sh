e6fdcf8204e16b4f469773f905ca8628c0994bc7
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-11 01:39:25 -0800

    unix version

--- scs/build.sh
+++ scs/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# on windows,
+# return {'libraries': ['C:\\Miniconda\\lib\\libf2c', 'C:\\Miniconda\\lib\\blas', 'C:\\Miniconda\\lib\\lapack',]}
+
+
+export LAPACK_INCLUDE_PATH=$LIBRARY_PATH
+export LAPACK_LIB_PATH=$INCLUDE_PATH
+export LAPACK_LIBS='f2c blas lapack'
 $PYTHON setup.py install
 
