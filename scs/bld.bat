a1f806d361a9f292b58f2155170702e76ebb53f8
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-11 14:25:45 -0800

    clapack
    Former-commit-id: 67efb65687a451dce907a7c7ebeb318cabdd7ed5

--- scs/bld.bat
+++ scs/bld.bat
@@ -1,2 +1,7 @@
+:: on windows,
+:: return {'libraries': ['C:\\Miniconda\\lib\\libf2c', 'C:\\Miniconda\\lib\\blas', 'C:\\Miniconda\\lib\\lapack',]}
+
+SET LAPACK_LIBS="%PREFIX%\\lib\\libf2c %PREFIX%\\lib\\blas %PREFIX%\\lib\\lapack"
+
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
