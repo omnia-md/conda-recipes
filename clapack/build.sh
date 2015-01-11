acb8f3873db3d1dfe76cd362c1d4186a3e490aff
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-11 15:24:12 -0800

    fPIC everywhere

--- clapack/build.sh
+++ clapack/build.sh
@@ -1,10 +1,9 @@
 mkdir build
 cd build
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -Wno-dev"
+CMAKE_FLAGS+=" -DCMAKE_C_FLAGS=-fPIC"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=''"
-else
-    CMAKE_FLAGS+=" -DCMAKE_C_FLAGS=-fPIC"
 fi
 
 cmake $CMAKE_FLAGS ..
