383854ec07138e4e91e2f66421d4b33094b0d281
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-01-11 00:23:27 +0000

    Add more files
    Former-commit-id: 2e9b8ceb9ea3204252e8dfdc09d7f975673ce2cc

---
+++ openmm/bld.bat
@@ -0,0 +1,20 @@
+:: Only python 3, since python2 uses VS2008,
+:: which is not capable of compiling OpenMM
+"%PYTHON%" -c "import sys; assert sys.version_info[0] == 3"
+if errorlevel 1 exit 1
+
+:: Build in subdirectory.
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
+cmake --build .
+msbuild ALL_BUILD.vcxproj
+msbuild INSTALL.vcxproj
+
+set OPENMM_INCLUDE_PATH=%PREFIX%\include
+set OPENMM_LIB_PATH=%PREFIX%\lib
+%PYTHON% setup.py install
+
+# Put examples into an appropriate subdirectory.
+mkdir %PREFIX%\share\openmm
+move %PREFIX%\examples %PREFIX%\share\openmm
