5d9ec74d10574280ba391b9ea11a18e31b02f4e7
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-16 14:28:25 -0800

    lol

--- ambermini/build.sh
+++ ambermini/build.sh
@@ -3,6 +3,9 @@
 # Required for cbio cluster for some stupid reason.
 #export LD_LIBRARY_PATH="/opt/gnu/gcc/4.8.1/lib64:/opt/gnu/gcc/4.8.1/lib:/opt/gnu/gmp/lib:/opt/gnu/mpc/lib:/opt/gnu/mpfr/lib"
 
+export CUSTOMBUILDFLAGS="-static-libgfortran /usr/local/Cellar/gcc/4.9.2_1/lib/gcc/4.9/libquadmath.a \
+	  -static-libgcc -lgfortran"
+
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
