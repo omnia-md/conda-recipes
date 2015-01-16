96825ad7b8eacd2c95501cf3ace92050ddb339e9
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-01-16 14:47:23 -0800

    Update build.sh

--- ambermini/build.sh
+++ ambermini/build.sh
@@ -3,8 +3,12 @@
 # Required for cbio cluster for some stupid reason.
 #export LD_LIBRARY_PATH="/opt/gnu/gcc/4.8.1/lib64:/opt/gnu/gcc/4.8.1/lib:/opt/gnu/gmp/lib:/opt/gnu/mpc/lib:/opt/gnu/mpfr/lib"
 
-export CUSTOMBUILDFLAGS="-static-libgfortran /usr/local/Cellar/gcc/4.9.2_1/lib/gcc/4.9/libquadmath.a \
-	  -static-libgcc -lgfortran"
+# See https://github.com/omnia-md/conda-recipes/pull/134
+#     https://github.com/omnia-md/conda-recipes/issues/132
+# This may require a patched version of gfortran to properly produce staticly linked binaries on osx
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   export CUSTOMBUILDFLAGS="-static-libgfortran /usr/local/Cellar/gcc/4.9.2_1/lib/gcc/4.9/libquadmath.a -static-libgcc -lgfortran"
+fi
 
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
