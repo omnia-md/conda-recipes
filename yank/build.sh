5b9b8e08ae81fb52c7b46a6439b04637cf4e9de5
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-01 21:00:43 -0500

    Updated yank recipe to also install examples, more closely match devtools version.
    Former-commit-id: 6c2127b342726db4e04bc805a675ad04078dc5fc

--- yank/build.sh
+++ yank/build.sh
@@ -1,1 +1,9 @@
-python setup.py install
+#!/bin/bash
+
+cp -r $RECIPE_DIR/../.. $SRC_DIR
+$PYTHON setup.py clean
+$PYTHON setup.py install
+
+# Push examples to anaconda/share/yank/examples/
+mkdir $PREFIX/share/yank
+cp -r $RECIPE_DIR/../../examples $PREFIX/share/yank/
