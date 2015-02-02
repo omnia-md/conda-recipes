25c93aefc79cfdaf40acb1cd43ea1d809e92a7f6
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-01 22:41:27 -0500

    Fixed relative path in yank build script.
    Former-commit-id: 2f5893a56bdfac487b705c79f73a0ecffd4bfe64

--- yank/build.sh
+++ yank/build.sh
@@ -5,4 +5,4 @@ $PYTHON setup.py install
 
 # Push examples to anaconda/share/yank/examples/
 mkdir $PREFIX/share/yank
-cp -r $RECIPE_DIR/../../examples $PREFIX/share/yank/
+cp -r examples $PREFIX/share/yank/
