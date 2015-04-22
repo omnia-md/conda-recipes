ee2dfb1fa2fe9f21201f05265d1c1e6e2bcecf4d
Robert McGibbon <rmcgibbo@gmail.com>
2015-04-22 16:39:24 -0700

    Add pyvisfile test

--- pyvisfile/build.sh
+++ pyvisfile/build.sh
@@ -1,2 +1,10 @@
-./configure.py --silo-inc-dir=$PREFIX/include --silo-lib-dir=$PREFIX/lib
+
+if [[ "$PY3K" == "1" ]]; then
+  boost_python_libname="boost_python3"
+  2to3 -w pyvisfile
+else
+    boost_python_libname="boost_python"
+fi
+
+./configure.py --silo-inc-dir=$PREFIX/include --silo-lib-dir=$PREFIX/lib --use-silo --boost-python-libname=$boost_python_libname
 python setup.py install
