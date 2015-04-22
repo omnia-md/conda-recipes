4c8ddd4a714cd8199b0216736d3d2738ac824c6f
Robert McGibbon <rmcgibbo@gmail.com>
2015-04-22 16:55:53 -0700

    Fix visfile osx

--- pyvisfile/build.sh
+++ pyvisfile/build.sh
@@ -6,5 +6,9 @@ else
     boost_python_libname="boost_python"
 fi
 
+UBLAS_INCLUDE=`python -c 'import pyublas; import os; print(os.path.join(os.path.dirname(str(pyublas.__file__)), "include"))'`
+NUMPY_INCLUDE=`python -c 'import numpy; print(numpy.get_include())'`
+export CFLAGS="-I$UBLAS_INCLUDE -I${NUMPY_INCLUDE}"
+
 ./configure.py --silo-inc-dir=$PREFIX/include --silo-lib-dir=$PREFIX/lib --use-silo --boost-python-libname=$boost_python_libname
 python setup.py install
