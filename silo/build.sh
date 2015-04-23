89a0cdfa19ce8032d9ccd3d478ec81494d95eada
Robert McGibbon <rmcgibbo@gmail.com>
2015-04-23 06:07:52 +0000

    Boost, pyublas, silo fixes for linux

--- silo/build.sh
+++ silo/build.sh
@@ -1,3 +1,17 @@
-./configure --with-hdf5=$PREFIX/include,$PREFIX/lib --prefix=$PREFIX --with-zlib=$PREFIX/include,$PREFIX/lib
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export FC=gfortran
+    export DEFAULT_HDF5_INCDIR=$INCLUDE_PATH
+    export DEFAULT_HDF5_LIBDIR=$LIBRARY_PATH
+    export CFLAGS="-I${INCLUDE_PATH} -fPIC"
+
+    ./configure --with-hdf5 --with-zlib --prefix=$PREFIX
+    for f in `find . -name Makefile`; do
+	sed "s/LIBS =  -lm -lsz -lhdf5 -lz -lsz/LIBS = -lm -lhdf5 -lz/g" -i $f
+    done
+else
+    ./configure --with-hdf5=$PREFIX/include,$PREFIX/lib --prefix=$PREFIX --with-zlib=$PREFIX/include,$PREFIX/lib
+fi
+
+
 make
 make install
