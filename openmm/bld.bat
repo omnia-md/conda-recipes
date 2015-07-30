d04d5ce42c15bcbf25a18ed4071c79aebe4179b7
Robert McGibbon <rmcgibbo@gmail.com>
2015-07-30 15:53:57 -0700

    OpenMM 6.3 windows
    Former-commit-id: 35132cd254eec444594dd2f928e5610fecee7a2b

--- openmm/bld.bat
+++ openmm/bld.bat
@@ -4,7 +4,9 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DFFTW_LIBRARY=%LIBRARY_LIB% -DFFTW_INCLUDES=%LIBARY_INC% -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DOPENMM_BUILD_PME_PLUGIN=ON -DFFTW_LIBRARY=%LIBRARY_LIB%\libfftwf-3.3.lib -DFFTW_INCLUDES=%LIBRARY_INC% -DCMAKE_BUILD_TYPE=Release ..
+
+jom all DoxygenApiDocs :: sphinxpdf
 jom install
 if errorlevel 1 exit 1
 
@@ -18,5 +20,12 @@ cd ..
 :: Put examples into an appropriate subdirectory.
 mkdir %PREFIX%\share\openmm
 move %PREFIX%\examples %PREFIX%\share\openmm
+
+:: Put docs into a subdirectory.
+cd %PREFIX%\docs
+mkdir openmm
+move *.html openmm
+:: move *.pdf openmm
+:: move api-* openmm
 
 if errorlevel 1 exit 1
