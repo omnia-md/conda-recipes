18bec02431b43c203d78eb335874f7c7257b8b57
Robert McGibbon <rmcgibbo@gmail.com>
2015-08-04 12:52:25 -0700

    Fix packmol for windows
    Former-commit-id: d5ef24bde5227dee8a45e10d3ed1a7d08280583f

--- packmol/bld.bat
+++ packmol/bld.bat
@@ -1,6 +1,8 @@
-
-bash .\configure C:\mingw64static\bin\gfortran.exe
-
+if "%ARCH%" == "32" (
+    bash .\configure C:\mingw32static\bin\gfortran.exe
+) ELSE (
+    bash .\configure C:\mingw64static\bin\gfortran.exe
+)
 
 :: http://superuser.com/questions/375029/make-interrupt-exception-caught
 python -c "a=open('Makefile').read(); f=open('Makefile', 'w'); f.write('SHELL=C:/Windows/System32/cmd.exe\n'); f.write(a)"
