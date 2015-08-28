9482c064e880805e59ef5f1d9c95605cd8cc7888
Robert McGibbon <rmcgibbo@gmail.com>
2015-08-27 18:52:48 -0700

    Faster

--- ambermini/bld.bat
+++ ambermini/bld.bat
@@ -1,4 +1,5 @@
 python configure --yacc=bison --cc gcc.exe --cxx g++.exe --fc gfortran.exe --prefix %PREFIX%
 mingw32-make
 mingw32-make install
-mv %PREFIX%\bin %PREFIX%\Scripts
+xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
+if errorlevel 1 exit 1
---
+++ jom/bld.bat
@@ -0,0 +1,4 @@
+mkdir %SCRIPTS%
+copy jom.exe %SCRIPTS%\
+
+if errorlevel 1 exit 1
