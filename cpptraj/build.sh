6128fd5ecbb3fa3be181633b6f4f3b4ae939a76f
Robert McGibbon <rmcgibbo@gmail.com>
2015-10-08 23:13:15 -0700

    Get it building on linux

--- cpptraj/build.sh
+++ cpptraj/build.sh
@@ -1,7 +1,14 @@
-export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
-./configure -fftw3 --with-netcdf=$PREFIX clang
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+    # OS X
+    export ORIG_DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
+    ./configure -fftw3 --with-netcdf=$PREFIX clang
+    export DYLD_LIBRARY_PATH=$ORIG_DYLD_LIBRARY_PATH
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+    ./configure -fftw3 --with-netcdf=$PREFIX gnu
+fi
 
-unset DYLD_LIBRARY_PATH
 make -j$CPU_COUNT install
-cp -R bin/ $PREFIX/bin/
-cp -R lib/ $PREFIX/lib/
+cp -R bin/* $PREFIX/bin/
