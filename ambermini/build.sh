e9eae44391abbaca0c065503ee6634c6cbe6bace
Robert McGibbon <rmcgibbo@gmail.com>
2015-10-10 23:27:52 -0700

    linux fix

--- ambermini/build.sh
+++ ambermini/build.sh
@@ -7,6 +7,15 @@ export LDFLAGS="-static-libgcc -lm \
 chmod u+x configure
 ./configure --prefix=$PREFIX
 
+# we deal with lgfortran through ldflags. don't want it
+# on config.h, where it overrides and leads to dynamic
+# linking on linux
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' 's/-lgfortran//g' config.h
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sed -i 's/-lgfortran//g' config.h
+fi
+
 # Build and install.
 make -j$CPU_COUNT
 chmod -R u+x bin
