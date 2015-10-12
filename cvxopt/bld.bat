6a382e0f53fda6ebf7670e740fd7fdd672b5b093
Robert McGibbon <rmcgibbo@gmail.com>
2015-10-12 02:14:39 -0700

    update cvxopt and use openblas

--- cvxopt/bld.bat
+++ cvxopt/bld.bat
@@ -1,26 +1,11 @@
-wget http://www.netlib.org/blas/blas.tgz
-tar -xf blas.tgz
-cd BLAS
-sed 's/_LINUX/_WIN/' make.inc -i
-mingw32-make && cp blas_WIN.a ../libblas.a
-cd ..
-if errorlevel 1 exit 1
+for /f "delims=" %%a in ('python %RECIPE_DIR%\libgfortranpath.py') do @set LIBGFORTRANPATH=%%a
 
-wget http://www.netlib.org/lapack/lapack-3.4.0.tgz
-tar -xf lapack-3.4.0.tgz
-cd lapack-3.4.0
-cp make.inc.example make.inc
-mingw32-make lapacklib && cp liblapack.a ..
-cd ..
-if errorlevel 1 exit 1
-
-cp libblas.a src/
-cp liblapack.a src/
-
-set CVXOPT_BLAS_EXTRA_LINK_ARGS=-lgfortran,-lquadmath
+xcopy %LIBRARY_LIB%\libopenblaspy.a .
+set CVXOPT_BLAS_LIB=openblaspy
+set CVXOPT_LAPACK_LIB=openblaspy
+set CVXOPT_BLAS_EXTRA_LINK_ARGS=-L%LIBGFORTRANPATH%,-lgfortran,-lquadmath
 set CVXOPT_BLAS_LIB_DIR=.
 
-%PYTHON% setup.py build --compiler=mingw32
-%PYTHON% setup.py install
+%PYTHON% setup.py build --compiler=mingw32 install
 %PYTHON% examples/book/chap4/portfolio.py
 if errorlevel 1 exit 1
