158c95a849494be3288670015bca59b8d229f2b8
Robert McGibbon <rmcgibbo@gmail.com>
2015-10-12 10:13:24 -0700

    openblas fixes

--- cvxopt/build.sh
+++ cvxopt/build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+
+export BLAS_LAPACK_LIB_PATHS=$PREFIX/lib
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export CVXOPT_BLAS_LIB=openblas
+    export CVXOPT_LAPACK_LIB=openblas
+    export CVXOPT_BLAS_EXTRA_LINK_ARGS=-lgfortran,-lquadmath
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export CVXOPT_BLAS_LIB=openblas
+    export CVXOPT_LAPACK_LIB=openblas
+    export CVXOPT_BLAS_EXTRA_LINK_ARGS=-lgfortran
+fi
+
 $PYTHON setup.py install
--- scs/build.sh
+++ scs/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-export BLAS_LAPACK_LIB_PATHS=$INCLUDE_PATH
-export BLAS_LAPACK_LIBS=f2c:blas:lapack
+
+export BLAS_LAPACK_LIB_PATHS=$PREFIX/lib
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export BLAS_LAPACK_LIBS=openblas:gfortran:quadmath
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export BLAS_LAPACK_LIBS=openblas:gfortran
+fi
+
 $PYTHON setup.py install
 
