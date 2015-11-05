a9a195d0f50017065a7e554b3e563381b91dfda8
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-05 15:51:58 -0800

    Bump OpenMM build number

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -4,10 +4,9 @@ set -e
 source /hbb_exe/activate
 
 set -x
-yum install -y git
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
-/io/conda-build-all --check-against omnia $UPLOAD /io/*
+/io/conda-build-all --check-against omnia $UPLOAD /io/openmm
