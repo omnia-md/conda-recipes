edcd916be278693dcae65531bc4db088cccf083d
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-05 15:59:06 -0800

    Add OpeNMM depdendencies

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -9,4 +9,4 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
-/io/conda-build-all --check-against omnia $UPLOAD /io/openmm
+/io/conda-build-all --check-against omnia $UPLOAD /io/*
