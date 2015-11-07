c88afdbd0f3cab57fb3c2d8bb7adc2f37b1d90c3
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-07 14:59:24 -0800

    Win packmol

--- packmol/bld.bat
+++ packmol/bld.bat
@@ -1,8 +1,9 @@
 bash .\configure %PREFIX%\Scripts\gfortran.exe
 
 :: http://superuser.com/questions/375029/make-interrupt-exception-caught
-python -c "a=open('Makefile').read(); f=open('Makefile', 'w'); f.write('SHELL=C:/Windows/System32/cmd.exe\n'); f.write(a)"
-make
+:: python -c "a=open('Makefile').read(); f=open('Makefile', 'w'); f.write('SHELL=C:/Windows/System32/cmd.exe\n'); f.write(a)"
+
+mingw32-make
 
 mkdir -p %PREFIX%\Scripts\
 cp packmol.exe %PREFIX%\Scripts\
