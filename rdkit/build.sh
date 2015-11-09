86b167c477decac7eeee9994443f4b399142e552
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-09 13:16:29 -0800

    RDKit: Fix typo in recipe

--- rdkit/build.sh
+++ rdkit/build.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-$PYTHON "$RECIPE_DIR/fetch_inchi.py"
-$PYTHON "$RECIPE_DIR/fetch_avalontools.py"
-PY_INC=`$PYTHON -c "from distutils import sysconfig; print (sysconfig.get_python_inc(0, '$PREFIX'))"`
+#$PYTHON "$RECIPE_DIR/fetch_inchi.py"
+#$PYTHON "$RECIPE_DIR/fetch_avalontools.py"
+#PY_INC=`$PYTHON -c "from distutils import sysconfig; print (sysconfig.get_python_inc(0, '$PREFIX'))"`
 
 cmake \
-
-    -D RDK_INSTALL_INTREE=OFF \
-    -D RDK_INSTALL_STATIC_LIBS=OFF \
-    -D RDK_BUILD_INCHI_SUPPORT=ON \
-    -D RDK_BUILD_AVALON_SUPPORT=ON \
-    -D RDK_USE_FLEXBISON=OFF \
-    -D RDK_BUILD_THREADSAFE_SSS=ON \
-    -D RDK_TEST_MULTITHREADED=ON \
-    -D AVALONTOOLS_DIR=$SRC_DIR/External/AvalonTools/src/SourceDistribution \
-    -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    -D Python_ADDITIONAL_VERSIONS=${PY_VER} \
-    -D PYTHON_EXECUTABLE=$PYTHON \
-    -D PYTHON_INCLUDE_DIR=${PY_INC} \
-    -D PYTHON_NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include \
-    -D BOOST_ROOT=$PREFIX -D Boost_NO_SYSTEM_PATHS=ON \
-    -D Boost_USE_STATIC_LIBS=ON \
-    -D CMAKE_BUILD_TYPE=Release \
+    -DRDK_INSTALL_INTREE=OFF \
+    -DRDK_INSTALL_STATIC_LIBS=OFF \
+    -DRDK_BUILD_INCHI_SUPPORT=ON \
+    -DRDK_BUILD_AVALON_SUPPORT=ON \
+    -DRDK_USE_FLEXBISON=OFF \
+    -DRDK_BUILD_THREADSAFE_SSS=ON \
+    -DRDK_TEST_MULTITHREADED=ON \
+    -DAVALONTOOLS_DIR=$SRC_DIR/External/AvalonTools/src/SourceDistribution \
+    -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DPython_ADDITIONAL_VERSIONS=${PY_VER} \
+    -DPYTHON_EXECUTABLE=$PYTHON \
+    -DPYTHON_INCLUDE_DIR=${PY_INC} \
+    -DPYTHON_NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include \
+    -DBOOST_ROOT=$PREFIX -D Boost_NO_SYSTEM_PATHS=ON \
+    -DBoost_USE_STATIC_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
     .
 
 make -j$CPU_COUNT install
