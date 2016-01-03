e743145279b2d8853cb9ba6043e352c68b747e9e
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-03 13:08:56 -0800

    Add openblas

--- cpptraj/build.sh
+++ cpptraj/build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
-    # OS X
     export ORIG_DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
     export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
-    ./configure --with-netcdf=$PREFIX gnu
+    ./configure -openblas --with-netcdf=$PREFIX gnu
     export DYLD_LIBRARY_PATH=$ORIG_DYLD_LIBRARY_PATH
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Linux
     export ORIG_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-    ./configure --with-netcdf=$PREFIX gnu
+    ./configure -openblas --with-netcdf=$PREFIX gnu
     export LD_LIBRARY_PATH=$ORIG_LD_LIBRARY_PATH
 fi
 
