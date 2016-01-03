979ab8e8d30936bd586e89641e369c91d574b56a
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-03 13:28:14 -0800

    Don't use openblas on OS X

--- cpptraj/build.sh
+++ cpptraj/build.sh
@@ -2,7 +2,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     export ORIG_DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
     export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
-    ./configure -openblas --with-netcdf=$PREFIX gnu
+    ./configure --with-netcdf=$PREFIX gnu
     export DYLD_LIBRARY_PATH=$ORIG_DYLD_LIBRARY_PATH
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export ORIG_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
