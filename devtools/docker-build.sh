6ebba15ddd9705b7197683f61ccb9a8130a9201f
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-01-08 22:48:28 -0500

    More fixes

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -3,6 +3,9 @@ set -e
 # Activate Holy Build Box environment.
 source /hbb_exe/activate
 
+# Disable PYTHONPATH
+unset PYTHONPATH
+
 set -x
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
