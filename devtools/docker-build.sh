f41bfa481fc806bdac11db3e7f2043bddf644bcd
Matthew Harrigan <harrigan@stanford.edu>
2016-05-03 14:01:03 -0700

    Check against omnia/label/rc

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -12,11 +12,12 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
+checkagainst='omnia omnia/label/pre omnia/label/rc'
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    /io/conda-build-all $UPLOAD --check-against omnia omnia/label/pre -- /io/* || true
+    /io/conda-build-all $UPLOAD --check-against $checkagainst -- /io/* || true
 else
-    /io/conda-build-all $UPLOAD --check-against omnia omnia/label/pre -- /io/*
+    /io/conda-build-all $UPLOAD --check-against $checkagainst -- /io/*
 fi
 
 #mv /anaconda/conda-bld/linux-64/*tar.bz2 /io/ || true
