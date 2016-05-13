f95d09ff126d8264d5e6d17b31369411da1d03ce
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-05-13 00:55:23 -0400

    Update SCS to latest version

--- scs/bld.bat
+++ scs/bld.bat
@@ -1,15 +1,2 @@
-REM Get the path where mingw's libgfortran.a and libquadmath.a are stored
-
-for /f "delims=" %%a in ('python %RECIPE_DIR%\libgfortranpath.py') do @set LIBGFORTRANPATH=%%a
-echo %foobar%
-
-REM Copy over the true static lib, libopenblaspy.a into the current directory,
-REM and then link to it here. Otherwise, if we use the directory %LIBRARY_LIB%,
-REM the linker will choose the import lib for the DLL.
-
-xcopy %LIBRARY_LIB%\libopenblaspy.a .
-set BLAS_LAPACK_LIB_PATHS=%LIBGFORTRANPATH%:.
-set BLAS_LAPACK_LIBS=openblaspy:gfortran:quadmath
-
 "%PYTHON%" setup.py build --compiler=mingw32 install
 if errorlevel 1 exit 1
