89578ae72cf353348386a8d25a63b783a2be42fc
Martin Scherer <m.scherer@fu-berlin.de>
2016-05-15 12:25:47 +0200

    [docker] use default labels to check against

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -12,12 +12,11 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
-checkagainst='omnia omnia/label/pre omnia/label/rc'
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    /io/conda-build-all $UPLOAD --check-against $checkagainst -- /io/* || true
+    /io/conda-build-all $UPLOAD -- /io/* || true
 else
-    /io/conda-build-all -v -v -v $UPLOAD --check-against $checkagainst -- /io/*
+    /io/conda-build-all -vvv $UPLOAD -- /io/*
 fi
 
 #mv /anaconda/conda-bld/linux-64/*tar.bz2 /io/ || true
