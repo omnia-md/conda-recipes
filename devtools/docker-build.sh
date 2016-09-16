dd1c8a42f795f95e14b5fef50c9f43c8ff2e70c6
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-09-16 09:51:33 +0200

    Try adding `source deactivate` as well

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -11,8 +11,7 @@ curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
-# conda-build 1.x appears to no longer be supported
-#conda install -yq conda-build=1.21.3 jinja2 anaconda-client
+source deactivate
 conda install -yq conda-build jinja2 anaconda-client
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
