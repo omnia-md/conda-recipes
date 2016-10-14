f1f2dae943bd571e5e18d5ba558e640a3743239c
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-10-13 22:42:07 -0400

    Update to CUDA 8.0

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -15,8 +15,8 @@ if ! ./conda-build-all --dry-run -- openmm* ; then
     # Install OpenMM dependencies that can't be installed through
     # conda package manager (doxygen + CUDA)
     brew install -y --quiet doxygen
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/7.5/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
+    curl -O -s http://developer.download.nvidia.com/compute/cuda/8.0/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
+    curl -O -s http://developer.download.nvidia.com/compute/cuda/8.0/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
     sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
     sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
     rm -f cuda_mac_installer_tk.tar.gz cuda_mac_installer_drv.tar.gz
