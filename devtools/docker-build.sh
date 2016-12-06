02f32575d6422848ff0abd9179c2c2605ce92bf4
Matthew Harrigan <harrigan@stanford.edu>
2016-12-06 11:48:19 -0800

    Update docker-build.sh
    This doesn't exist, as far as I can tell

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -9,7 +9,7 @@ unset PYTHONPATH
 set -x
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
+PATH=/opt/rh/devtoolset-2/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
 conda install -yq conda-build=$CONDA_BUILD jinja2 anaconda-client
 
