f07395263d167a5fd0529131cfe2ae15d3eb7cf6
Matthew Harrigan <harrigan@stanford.edu>
2017-01-08 15:53:46 -0800

    Updates for anvil image and new conda build

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
-# Activate Holy Build Box environment.
-source /hbb_exe/activate
-
-# Disable PYTHONPATH
-unset PYTHONPATH
-
 set -x
-curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-PATH=/opt/rh/devtoolset-2/root/usr/bin:/anaconda/bin:$PATH
+
+conda config --remove channels conda-forge
 conda config --add channels omnia
 conda install -yq conda-build jinja2 anaconda-client
 
