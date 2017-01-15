187f3c47fef5d45dd1ff79044138b4f7b32a9ee3
John Chodera (MSKCC) <choderaj@mskcc.org>
2017-01-15 13:33:52 -0800

    Fix up osx build logic

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -11,9 +11,12 @@ curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.s
 bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels omnia;
+conda config --show;
 conda install -yq conda-build jinja2 anaconda-client;
 
-if ! ./conda-build-all --dry-run -- openmm* ; then
+#export INSTALL_CUDA=`./conda-build-all --dry-run -- openmm`
+export INSTALL_OPENMM_PREREQUISITES=true
+if [ "$INSTALL_OPENMM_PREREQUISITES" = true ] ; then
     # Install OpenMM dependencies that can't be installed through
     # conda package manager (doxygen + CUDA)
     brew install -y --quiet doxygen
@@ -22,13 +25,13 @@ if ! ./conda-build-all --dry-run -- openmm* ; then
     sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
     sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
     rm -f cuda_mac_installer_tk.tar.gz cuda_mac_installer_drv.tar.gz
-fi;
 
-# Install latex.
-brew cask install -y basictex
-export PATH="/usr/texbin:${PATH}:/usr/bin"
-sudo tlmgr update --self
-sudo tlmgr install titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring
+    # Install latex.
+    brew cask install -y basictex
+    export PATH="/usr/texbin:${PATH}:/usr/bin"
+    sudo tlmgr update --self
+    sudo tlmgr install titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring
+fi;
 
 # Build packages
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
