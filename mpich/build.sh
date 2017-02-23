519a9387f33e23b27a3da1af3b05aa1eae68fab1
Levi Naden <levi.naden@choderalab.org>
2017-02-23 15:29:05 -0500

    Changed to using the feedstock instead of the original PR that was used to make it

--- mpich/build.sh
+++ mpich/build.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-if [[ $(uname) == Linux ]]; then
-  # FIXME: This is a terrible workaround.
-  # Ideally we should fix the information the .la files.
-  rm -rf $PREFIX/lib/libquadmath.la
-  rm -rf $PREFIX/lib/libgfortran.la
-  OPTS="--enable-fortran=yes"
-elif [[ $(uname) == Darwin ]]; then
-  OPTS="--enable-fortran=no"
+if [ $(uname) == Darwin ]; then
+    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
 fi
 
+export LIBRARY_PATH="$PREFIX/lib"
+
 ./configure --prefix=$PREFIX \
-            --enable-shared \
-            $OPTS
+            --disable-dependency-tracking \
+            --enable-cxx \
+            --enable-fortran
 
 make
-make testing
 make install
-
-cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
