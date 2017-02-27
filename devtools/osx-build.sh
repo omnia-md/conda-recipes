779d9006a0863e144e30a9c073828ae6ea2d3afc
Levi Naden <levi.naden@choderalab.org>
2017-02-27 15:30:26 -0500

    Manually install fncychap LatTeX package to build OpenMM docs

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -31,7 +31,7 @@ if [ "$INSTALL_OPENMM_PREREQUISITES" = true ] ; then
     export PATH="/usr/texbin:${PATH}:/usr/bin"
     sudo tlmgr update --self
     sleep 5
-    sudo tlmgr --persistent-downloads install titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring
+    sudo tlmgr --persistent-downloads install titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring fncychap
 fi;
 
 # Build packages
