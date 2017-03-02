a7a607551485a9f6f51285317677427a28b70319
Levi Naden <levi.naden@choderalab.org>
2017-03-02 10:00:19 -0500

    Add a search for the libgfortran lib

--- cvxopt/build.sh
+++ cvxopt/build.sh
@@ -2,6 +2,7 @@
 
 
 export BLAS_LAPACK_LIB_PATHS=$PREFIX/lib
+export LDFLAGS=$(python libgfortranpath.py)
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export CVXOPT_BLAS_LIB=openblas
     export CVXOPT_LAPACK_LIB=openblas
