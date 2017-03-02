57bdebd0cb118481f35f9718e9bd07afc6625656
Levi Naden <levi.naden@choderalab.org>
2017-03-02 12:05:50 -0500

    Try linking to lib path to get the libfortran flags

--- cvxopt/build.sh
+++ cvxopt/build.sh
@@ -2,7 +2,7 @@
 
 
 export BLAS_LAPACK_LIB_PATHS=$PREFIX/lib
-export LDFLAGS=$(python libgfortranpath.py)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"${PREFIX}/lib"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export CVXOPT_BLAS_LIB=openblas
     export CVXOPT_LAPACK_LIB=openblas
