831fd41b3758581d52d39840679650a5832ecf30
Levi Naden <levi.naden@choderalab.org>
2017-04-17 11:20:28 -0400

    Update docker file Initial README update (still WIP)

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
-# Activate Holy Build Box environment.
-source /hbb_exe/activate
-
-# Disable PYTHONPATH
-unset PYTHONPATH
-
 set -x
-curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-PATH=/opt/rh/devtoolset-2/root/usr/bin:/anaconda/bin:$PATH
 conda config --add channels omnia
-conda install -yq conda-build=2.1.5 jinja2 anaconda-client
-# Install missing LaTeX docs
-tlmgr install fncychap tabulary capt-of eqparbox environ trimspaces
+conda install -yq conda-build jinja2 anaconda-client
 
 /io/conda-build-all -vvv $UPLOAD -- /io/*
 
