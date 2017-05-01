fe657426cd380830989ced76ec81c1876db78401
Levi Naden <levi.naden@choderalab.org>
2017-05-01 15:35:45 -0400

    Fix the version check

--- openmm/run_test.py
+++ openmm/run_test.py
@@ -3,7 +3,8 @@
 from simtk import openmm
 
 # Check major version number
-assert openmm.Platform.getOpenMMVersion() == '7.1', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
+# If Z=0 for version X.Y.Z, out put is "X.Y"
+assert openmm.Platform.getOpenMMVersion() == '7.1.1', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
 assert openmm.version.git_revision == '4b6fad2c19ea87a117b37969e96c99ffcbcf38e3', "openmm.version.git_revision = %s" % openmm.version.git_revision
