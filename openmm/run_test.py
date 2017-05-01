ac58f396346af575d2100b1699cc63b4a953d58e
Levi Naden <levi.naden@choderalab.org>
2017-05-01 12:52:51 -0400

    Re-enable git hash since no longer using a patch to fix the build. Update git hash that is being checked against

--- openmm/run_test.py
+++ openmm/run_test.py
@@ -6,5 +6,4 @@ from simtk import openmm
 assert openmm.Platform.getOpenMMVersion() == '7.1', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
-# TODO: Reactivate when no longer using a patch to publish docs from 7.1
-# assert openmm.version.git_revision == '9567ddb304c48d336e82927adf2761e8780e9270', "openmm.version.git_revision = %s" % openmm.version.git_revision
+assert openmm.version.git_revision == '4b6fad2c19ea87a117b37969e96c99ffcbcf38e3', "openmm.version.git_revision = %s" % openmm.version.git_revision
