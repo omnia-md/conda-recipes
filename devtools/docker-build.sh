c8fff7270f58ce7160ea03cc727ecd42376ba011
Levi Naden <levi.naden@choderalab.org>
2017-05-23 16:42:39 -0400

    Made conda-forge top priority channel, omnia second Removed --no-chan-pri requirement Updated README to reflect changes Added "How to keep using only omnia" info to README

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 set -x
-conda config --set channel_priority false
 conda config --add channels omnia
+# Move the conda-forge channel to the top
+# Cannot just append omnia otherwise default would have higher priority
+conda config --add channels conda-forge
 conda install -yq conda-build jinja2 anaconda-client
 
 /io/conda-build-all -vvv $UPLOAD -- /io/*
