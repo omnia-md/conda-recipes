c031db66d2e94bd7a043db5295e92f3a3ecdc14b
John Chodera <john.chodera@choderalab.org>
2018-12-31 21:25:22 -0800

    Be sure to check against omnia main and cudaXY labels for osx builds

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -87,5 +87,5 @@ conda config --add channels omnia/label/betacuda${CUDA_SHORT_VERSION};
 
 #for PY_BUILD_VERSION in "27" "35" "36" "37"; do
 for PY_BUILD_VERSION in "37" "36" "35" "27"; do
-    ./conda-build-all -vvv --python $PY_BUILD_VERSION --check-against omnia/label/beta --check-against omnia/label/betacuda${CUDA_SHORT_VERSION} --check-against omnia/label/dev --check-against omnia/label/devcuda${CUDA_SHORT_VERSION} --numpy "1.15" $UPLOAD -- *
+    ./conda-build-all -vvv --python $PY_BUILD_VERSION --check-against omnia --check-against omnia/label/cuda${CUDA_SHORT_VERSION} --check-against omnia/label/beta --check-against omnia/label/betacuda${CUDA_SHORT_VERSION} --numpy "1.15" $UPLOAD -- *
 done
