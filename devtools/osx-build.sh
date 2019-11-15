da6a10f00c0fd4c08b32a84996afbf3038ae1649
Jeff Wagner <jwagnerjpl@gmail.com>
2019-11-15 11:06:37 -0800

    test whether py38 builds are possible

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -89,4 +89,4 @@ conda config --add channels omnia/label/betacuda${CUDA_SHORT_VERSION};
 #for PY_BUILD_VERSION in "37" "36" "35" "27"; do
 #    ./conda-build-all -vvv --python $PY_BUILD_VERSION --check-against omnia --check-against omnia/label/cuda${CUDA_SHORT_VERSION} --check-against omnia/label/beta --check-against omnia/label/betacuda${CUDA_SHORT_VERSION} --numpy "1.15" $UPLOAD -- *
 #done
-./conda-build-all -vvv --python "37,36,27" --numpy "1.14,1.15" $UPLOAD -- *
+./conda-build-all -vvv --python "38,37,36,27" --numpy "1.14,1.15" $UPLOAD -- *
